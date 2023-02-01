b8836434e16436a46ddbfec87f56ddce217ee1e3
vannagrim-UTD2020 <61626082+vannagrim-UTD2020@users.noreply.github.com>
2023-02-01 11:26:12 -0600

    Python Projects

---
+++ Python/QRCodeGenerator/QRgenerator.py
@@ -0,0 +1,26 @@
+# Quick Project
+# install libraries
+# make the function that collects the link and generate a qr code
+# save QR code as an image
+# run it all
+
+import qrcode
+
+def generate_qrcode(text):
+    
+    qr = qrcode.QRCode(
+    version = 1,
+    error_correction = qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+    )
+    
+    
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrimg.png")
+    
+
+url = input("Enter your url:")
+generate_qrcode(url)
